b0c5e6c4c1efdc0af34feccdea33e220078d1b01
Nicholas <nicholas.guilbeault@mail.utoronto.ca>
2018-12-13 23:17:56 -0500

    Modified head fixed tracking algorithm to search for swim bladder before background subtraction.

--- main.py
+++ main.py
@@ -3640,12 +3640,12 @@ class TrackVideoThread(QThread):
                                             second_eye_angle = cv2.fitEllipse(contours[i])[2] * np.pi / 180
                                 # Find the midpoint of the line that connects both eyes.
                                 heading_coords = [(first_eye_coords[0] + second_eye_coords[0]) / 2, (first_eye_coords[1] + second_eye_coords[1]) / 2]
+                                # Find the swim bladder coordinates by finding the next brightest coordinates that lie on a circle around the heading coordinates with a radius equal to the distance between the eyes and the swim bladder.
+                                swim_bladder_coords = ut.calculate_next_coords(heading_coords, dist_swim_bladder, frame, n_angles = 100, range_angles = 2 * np.pi, tail_calculation = False)
                                 # Convert the frame into the absolute difference between the frame and the background.
                                 frame = cv2.absdiff(frame, background)
                                 # Apply a median blur filter to the frame.
                                 frame = cv2.medianBlur(frame, median_blur)
-                                # Find the swim bladder coordinates by finding the next brightest coordinates that lie on a circle around the heading coordinates with a radius equal to the distance between the eyes and the swim bladder.
-                                swim_bladder_coords = ut.calculate_next_coords(heading_coords, dist_swim_bladder, frame, n_angles = 100, range_angles = 2 * np.pi, tail_calculation = False)
                                 # Find the body coordinates by finding the center of the triangle that connects the eyes and swim bladder.
                                 body_coords = [int(round((swim_bladder_coords[0] + first_eye_coords[0] + second_eye_coords[0]) / 3)), int(round((swim_bladder_coords[1] + first_eye_coords[1] + second_eye_coords[1]) / 3))]
                                 # Calculate the heading angle as the angle between the body coordinates and the heading coordinates.
